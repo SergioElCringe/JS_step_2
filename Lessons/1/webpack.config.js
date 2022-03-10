ad0941495dfb1e0f9cf6b659f8332562522750e6
kellolo <87kell@gmail.com>
2022-03-10 20:26:28 +0200

    l1

---
+++ Lessons/1/webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+      chunkFilename: "[id].css",
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: './src/assets/img', to: 'assets/img' }
+      ]
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [{
+          loader: MiniCssExtractPlugin.loader,
+        }, 'css-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          // options: {
+          //   presets: ['@babel/preset-env']
+          // }
+        }
+      }
+    ]
+  },
+  devServer: {
+    hot: true,
+    open: true,
+  },
+};
