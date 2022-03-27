4037b53331301936a5c089d095ec57d09ab33ad1
Kungurov_Denis <denkungurow@yandex.ru>
2022-03-27 22:15:29 +0400

    Kungurov_hw_2

---
+++ STUDENTS/Kungurov/KungurovHw_2/prog/src/index.js
@@ -0,0 +1,34 @@
+import './styles/cart_responsive.css'
+import './styles/cart.css'
+import './styles/categories_responsive.css'
+import './styles/categories.css'
+import './styles/checkout_responsive.css'
+import './styles/checkout.css'
+import './styles/contact_responsive.css'
+import './styles/contact.css'
+import './styles/main_styles.css'
+import './styles/product_responsive.css'
+import './styles/product.css'
+import './styles/responsive.css'
+//import './styles/f-a/css/font-awesome.min.css'
+
+import app from './js/components/app'
+import cart from './js/components/cart'
+import catalog from './js/components/catalog'
+import topNav from './js/components/topNav'
+
+
+const car = new cart();
+const cat = new catalog();
+const nav = new topNav();
+
+car;
+cat;
+nav;
+console.log(car);
+ //iport $ from 'jquery'
+import 'bootstrap/dist/css/bootstrap.min.css'
+ //import bootstrap from 'bootstrap'
+
+
+//app();
