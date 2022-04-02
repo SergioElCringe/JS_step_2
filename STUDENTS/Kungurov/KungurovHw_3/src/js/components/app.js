50ba4457c786305222cc5bbf48aa250ec4cbbd51
Kungurov_Denis <denkungurow@yandex.ru>
2022-04-03 00:34:11 +0400

    Kungurov_hw_3

---
+++ STUDENTS/Kungurov/KungurovHw_3/src/js/components/app.js
@@ -0,0 +1,8 @@
+
+import Cart from "./general/Cart";
+import Catalog from "./general/Catalog";
+
+export default function () {
+    const cart = new Cart();
+    const catalog = new Catalog (cart);
+}
---
+++ STUDENTS/Kungurov/KungurovHw_3/src/js/components/general/Catalog.js
@@ -0,0 +1,36 @@
+import List from "./LIST"
+const url = 'https://raw.githubusercontent.com/schultznoan/FTP/main/fetchData/catalog.json';
+
+
+export default class Catalog extends List {
+  constructor(cart, type) {
+    super(url, type = 'catalog')
+    this.cart = cart;
+    this.container = null;
+  }
+  _initContainers() {
+    this.container = document.querySelector('#catalog');
+    this._render();
+    this._handleEvents();
+  }
+  _handleEvents(evt) {
+    this.container.addEventListener('click', evt => {
+      if (evt.target.classList.contains('btn-add')) {
+        const { name, price, imgurl, id } = evt.target.dataset;
+        this.cart.addItem({
+          name, price, id,
+          imgUrl: imgurl
+        });
+      }
+    });
+  }
+  getProductExtraTemplate(categoryId) {
+    const category = categoryId ? this.getCategory(categoryId) : null;
+    return category ? `
+      <div class="product_extra ${category.className}">
+        <a href="categories.html">${category.text}</a>
+      </div>
+    ` : '';
+  }
+
+}
