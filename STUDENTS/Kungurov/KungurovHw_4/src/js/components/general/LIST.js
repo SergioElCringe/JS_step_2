7405eeab8a51826ef42f68624803a0764ca9e395
Kungurov_Denis <denkungurow@yandex.ru>
2022-04-03 16:34:56 +0400

    KungurovHw_4

---
+++ STUDENTS/Kungurov/KungurovHw_4/src/js/components/general/LIST.js
@@ -0,0 +1,47 @@
+import Item from "./LIST_ITEM";
+export default class List {
+    constructor(url, type) { 
+        this.items = [];
+        this.url = url;
+        this.info = null;
+        this.prices = [];
+        this._init();
+        this.container = [];
+        this.cart = '';
+    }
+    async _init() {
+        try {
+            this.items = await this._fetchData();
+          }
+          catch(err) {
+            this.error = err;
+          }
+          finally {
+            this._initContainers();
+            if (!this.error) {
+              this._render();
+              if (this.items.length) {
+              }
+            }
+          }
+    }
+
+    _render() { 
+        let result = '';
+
+        this.items.forEach(item => {
+            const newItem = new Item(item, this.type)
+            result += newItem.template;   
+        });
+
+        this.container.innerHTML = result;
+    }
+
+
+    _fetchData() {
+        return fetch(this.url)
+                .then(d => d.json())
+                .catch(e => { throw e })
+    }
+
+}
