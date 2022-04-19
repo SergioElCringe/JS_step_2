f0a210975cfb4b5bd0bd6a3dac4ec5961dc5d3a2
Kungurov_Denis <denkungurow@yandex.ru>
2022-04-19 21:02:05 +0400

    VueKung_2

---
+++ STUDENTS/Kungurov/VueKung_2/src/js/app.js
@@ -0,0 +1,7 @@
+import catalog from "./VueComponents/catalog/Catalog";
+import cart from "./VueComponents/cart/cart";
+export default({
+
+    el: '#app',
+    components: { cart , catalog  },
+}); 
