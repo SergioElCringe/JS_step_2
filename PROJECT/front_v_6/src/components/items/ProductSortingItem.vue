e489396c51402f5a04d85bf4ced2585797752889
kellolo <87kell@gmail.com>
2022-04-27 20:46:55 +0200

    axios

---
+++ PROJECT/front_v_6/src/components/items/ProductSortingItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <li class="product_sorting_btn">
+    <span>{{ name }}</span>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'ProductSortingItem',
+  props: {
+    item: {
+      type: Object,
+    },
+  },
+
+  computed: {
+    name() {
+      return this.item.value.toUpperCase();
+    },
+  },
+};
+</script>
