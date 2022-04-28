d8cd019953e8e8dfd5a3a5bec5a58d8677c904ae
kellolo <87kell@gmail.com>
2022-04-28 18:25:22 +0200

    Merge pull request #56 from SergioElCringe/VUE-(Eminov-hw-3)
    VUE HW-3 is done (by Rustam)

---
+++ STUDENTS/Eminov/Project/PRODUCT/src/src/components/Menu.vue
@@ -0,0 +1,45 @@
+<template>
+  <ul>
+    <menuItem
+      v-for="item of items"
+      :key="item.id"
+      :item="item"
+      :api="api"
+    />
+  </ul>
+</template>
+
+<script>
+import menuItem from './items/MenuItem.vue'
+export default {
+  name: "MenuNavigation",
+  components: { menuItem },
+
+  data() {
+    return {
+      items: [],
+      api: {
+        menuApi: 'https://raw.githack.com/SergioElCringe/JS_step_1/main/BASE__PROJECT',
+        url: '/api/menu'
+      },
+    };
+  },
+
+  methods: {
+    fetchMenu() {
+      try {
+        this.items = await $api.send(this.api.url, "GET");
+      } catch (err) {
+        console.log(err);
+      };
+    }
+  },
+
+  created() {
+    this.fetchMenu(); 
+  },
+};
+</script>
+
+<style>
+</style>
