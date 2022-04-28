3272c41e837f8404ec6917e8fc16fc7c85518816
Rustam Schultz <danchoo14@bk.ru>
2022-04-28 09:02:35 +0300

    Исправление конфликтов

--- STUDENTS/Eminov/Project/PRODUCT/src/index.js
+++ STUDENTS/Eminov/Project/PRODUCT/src/index.js
@@ -1,8 +1,8 @@
 import './src/styles/main_styles.css';
 import app from './src/js/app';
-// import RequestManager from './src/js/components/api/index';
+import RequestManager from './src/js/components/api/index';
 
-// const request = new RequestManager();
-// app(request);
+const request = new RequestManager();
 
+Vue.prototype.$api = request;
 const project = new Vue(app);
