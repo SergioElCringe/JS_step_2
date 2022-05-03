54f9596c72c992daee1dceef6c01e6c06b4e9380
Mikhail <mr.nimostor@mail.ru>
2022-05-03 19:31:23 +0300

    cart and catalog store modules

---
+++ STUDENTS/Makarov/Makarov-vue-5/project-vue-cli/src/components/Cart/CartItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class='cart-item'>
+    <img class='cart-item__img' :src='imgURLTemplate + item.imgFileName' />
+    <div class='cart-item__info'>
+      <span class='cart-item__title'>{{ item.title }}</span>
+      <div class='cart-item__order'>
+        <span class='cart-item__price'>{{ item.price }}</span>
+        <span class='cart-item__amount'>
+          <b class='cart-item__btn cart-item__btn_minus'>-</b>
+          <span> {{ item.amount }} </span>
+          <b class='cart-item__btn cart-item__btn_plus'>+</b>
+        </span>
+      </div>
+    </div>
+    <button class='cart__delete-btn'  @click='removeItem(item.id)'>×</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CartItem',
+  props: ['item', 'imgURLTemplate'],
+  methods: {
+    async removeItem(id) {
+      
+    }
+  }
+};
+</script>
+
+<style>
+</style>
