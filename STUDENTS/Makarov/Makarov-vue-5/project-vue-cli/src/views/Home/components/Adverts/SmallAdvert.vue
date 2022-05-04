92570deebfad5fe629b7c3ce77b2662769655276
Mikhail <mr.nimostor@mail.ru>
2022-05-04 17:51:39 +0300

    Makarov-vue-5

---
+++ STUDENTS/Makarov/Makarov-vue-5/project-vue-cli/src/views/Home/components/Adverts/SmallAdvert.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="avds_small">
+        <div class="avds_background"
+            style="background-image:url(https://raw.githubusercontent.com/SergioElCringe/JS_step_1/main/TEST_FTP/static/avds_small.jpg)">
+        </div>
+        <div class="avds_small_inner">
+            <div class="avds_discount_container">
+                <img
+                    src="https://raw.githubusercontent.com/MikhailErnstovich/my-ftp/master/img/discount.png">
+                <div>
+                    <div class="avds_discount">
+                        <div>20<span>%</span></div>
+                        <div>Discount</div>
+                    </div>
+                </div>
+            </div>
+            <div class="avds_small_content">
+                <div class="avds_title">Smart Phones</div>
+                <div class="avds_link">
+                    <a href="categories.html">See More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SmallAdvert',
+}
+</script>
+
+<style>
+
+</style>
