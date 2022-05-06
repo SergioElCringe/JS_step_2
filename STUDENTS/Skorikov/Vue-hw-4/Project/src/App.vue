ee6e8ca1f6da09f6028ff0e12a7270b6bab2d7ca
Vl007 <skorikov777@yandex.ru>
2022-05-06 17:27:20 +0300

    vue hw 4 is done

---
+++ STUDENTS/Skorikov/Vue-hw-4/Project/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="super_container">
+
+    <Header />
+
+    <router-view />
+
+    <Footer />
+    
+  </div>
+</template>
+
+<script>
+import Header from './components/pages/Header.vue';
+import Footer from './components/pages/Footer.vue';
+
+export default {
+  name: 'app',
+  components: { Header, Footer } 
+};
+</script>
+
+<style lang="scss">
+
+</style>
