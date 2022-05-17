a8118c6ba314b801b1a0d230837780e26f4d93de
lgnvk <k.s.loginoff@gmail.com>
2022-05-17 22:28:29 +0400

    Loginov-vue-1

---
+++ STUDENTS/Loginov/Vue/Loginov-vue-1/Project/src/index.js
@@ -0,0 +1,25 @@
+import './styles/cart_responsive.css'
+import './styles/cart.css'
+import './styles/categories_responsive.css'
+import './styles/categories.css'
+import './styles/checkout_responsive.css'
+import './styles/checkout.css'
+import './styles/contact_responsive.css'
+import './styles/contact.css'
+import './styles/main_styles.css'
+import './styles/product_responsive.css'
+import './styles/product.css'
+import './styles/responsive.css'
+import './styles/f-a/css/font-awesome.min.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+// import RequestManager from './js/components/api';
+// const request = new RequestManager();
+// app(request);
+
+
+import app from './js/app';
+const project = new Vue(app);
+
