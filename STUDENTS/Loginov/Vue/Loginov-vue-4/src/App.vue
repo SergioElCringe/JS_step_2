366b84f76be53c6c11734a46060438e683efc0a7
lgnvk <k.s.loginoff@gmail.com>
2022-05-28 20:36:36 +0400

    Loginov-vue-4

---
+++ STUDENTS/Loginov/Vue/Loginov-vue-4/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="super_container">
+    <!-- Header -->
+    <Header />
+    <!-- Home -->
+    
+    <router-view />
+
+    <!-- Footer -->
+    <Footer />
+    
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+export default {
+  components: {
+    Header,
+    Footer
+  },
+  // data() {
+  //   return {
+  //   }
+  // },
+  // created() {
+  // },
+};
+</script>
+
+<style lang="scss">
+</style>
