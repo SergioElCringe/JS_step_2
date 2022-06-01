389f8bb950a896866038a69ff2d0e9430c896455
AndrewArkhipov <AndrewArlhipow@yandex.ru>
2022-06-01 16:36:45 +0300

    fix "запасы на зиму"

--- STUDENTS/Arkhipov/Vue/1/src/index.js
+++ STUDENTS/Arkhipov/Vue/1/src/index.js
@@ -1,10 +1,3 @@
-// import './styles/main_styles.css'
-// import './styles/my_style.css'
-// import app from './js/app'
-// import RequestManager from './js/components/api';
-// const request = new RequestManager();
-// app(request);
-
 const app = new Vue({
 	el: '#app',
 
