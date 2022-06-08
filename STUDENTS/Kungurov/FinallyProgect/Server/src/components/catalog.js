a9ffaefcf8f133ccb393571b82a25a6dad03b6cf
Kungurov_Denis <denkungurow@yandex.ru>
2022-06-08 20:04:54 +0400

    Finaly progect

--- STUDENTS/Kungurov/FinallyProgect/Server/src/components/catalog.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    findItem(catalog, id) {
-        return catalog.find (el => el.id === id);
-    },
-    filter(catalog, param) {
-        //const params = Object.keys(params);
-        return !param ? catalog : catalog.filter(el => el[param]);
-    },
-}
