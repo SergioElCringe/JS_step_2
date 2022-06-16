71270410ed5d9fb82c13976de1898014f654d756
Mikhail <mr.nimostor@mail.ru>
2022-06-16 12:39:13 +0300

    Revert "draft version"
    This reverts commit 226ddbc913bac66d6b8e9ac15b3e5f2a26484316.

--- STUDENTS/Makarov/Makarov-graduation-project/Sublime-web-store/src/api/modules/index.js
+++
@@ -1,7 +0,0 @@
-import { catalog } from "./catalog.js";
-import { cart } from './cart.js'
-import { nav } from './nav.js'
-
-export { 
-  catalog, cart, nav
-};
--- STUDENTS/Makarov/Makarov-graduation-project/Sublime-web-store/src/main.js
+++
@@ -1,9 +0,0 @@
-import './assets/styles/main_styles.css';
-import './assets/styles/responsive.css';
-
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
